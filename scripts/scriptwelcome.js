cc2bc4fab5a5287519fdef2f3930fa54cce228c6
Luigi Carannante <luigicarannante9802@libero.it>
2024-09-03 10:32:58 +0200

    welcome css

--- scripts/scriptwelcome.js
+++ scripts/scriptwelcome.js
@@ -1,3 +1,12 @@
-const checkbox = document.getElementById("checkbox-luigi");
-const button = document.getElementById("button-luigi");
-const illuminated = document.querySelector("");
+/*const checkbox = document.getElementsByClassName(".checkbox-luigi");
+const button = document.getElementsByClassName(".button-luigi");
+const illuminated = document.getElementsByClassName(".button-luigi:active");
+
+checkbox.addEventListener("changes", function(){
+    button.disabled = !this.checked;
+    if(this.checked){
+        button.
+    }
+    
+}
+)
