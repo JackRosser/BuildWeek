c55a5c21d72be08c536a97235aea9cec518fba3d
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2024-09-03 11:55:30 +0200

    update/fix css remove chart, add conic-gradient fix js

--- assets/scripts/resultsJS.js
+++ assets/scripts/resultsJS.js
@@ -5,49 +5,31 @@ const chartData = {
 // quante corrette e sbagliate
 let corrette = 0;
 let sbagliate = 0;
+let valGrafico = 0;
 
 // funzione che calcola la percentuale in base alle domande corrette
 function calcDomande(numC, numS) {
   corrette = ((numC / 6) * 100).toFixed(1);
   sbagliate = ((numS / 6) * 100).toFixed(1);
 }
-
 calcDomande(4, 2);
+
+valGrafico = Math.ceil((360 * sbagliate) / 100);
+console.log(valGrafico);
+
 //  inserisco i dati nelle risorse del grafico
 chartData.data[0] = sbagliate;
 chartData.data[1] = corrette;
 
-const percentCorrect = document.querySelectorAll(".percent span");
+const percentCorrect = document.querySelectorAll(".percent");
 percentCorrect[0].innerHTML = corrette + "%";
 percentCorrect[1].innerHTML = sbagliate + "%";
 console.log(percentCorrect[0].innerHTML);
 
 // creazione grafico a donut
-const myChart = document.getElementById("myChart");
-
-new Chart(myChart, {
-  type: "doughnut",
-  data: {
-    labels: chartData.labels,
-    datasets: [
-      {
-        data: chartData.data,
-        backgroundColor: ["#C2128D", "#00FFFF"],
-        borderWidth: 0,
-      },
-    ],
-  },
-  options: {
-    responsive: false,
-    maintainAspectRatio: false, // Rende il grafico più flessibile per l'adattamento alle dimensioni del contenitore
-    cutout: "70%", // Dimensione del buco interno per fare il donut
-    plugins: {
-      tooltip: {
-        enabled: false, // Disabilita il tooltip
-      },
-    },
-  },
-});
+const grafico = document.getElementById("grafico");
+grafico.style.background = `conic-gradient(#d20094 ${valGrafico}deg, #00ffff ${valGrafico}deg 360deg)`;
+console.log(grafico);
 
 // il bottone ti porta alla pagina di feedback
 const rateUs = document.getElementById("rateUs");
