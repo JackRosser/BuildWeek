0f41407e7d9b194597f441789ec2a3fd94b34524
Luigi Carannante <luigicarannante9802@libero.it>
2024-09-03 17:55:59 +0200

    welcome java in progress

--- scripts/scriptwelcome.js
+++ scripts/scriptwelcome.js
@@ -1,12 +1,22 @@
-/*const checkbox = document.getElementsByClassName(".checkbox-luigi");
-const button = document.getElementsByClassName(".button-luigi");
-const illuminated = document.getElementsByClassName(".button-luigi:active");
+const promise = document.getElementById("promise");
+const button = document.getElementById("buttonLuigi");
+const contenitore = document.querySelector(".container");
 
-checkbox.addEventListener("changes", function(){
-    button.disabled = !this.checked;
-    if(this.checked){
-        button.
-    }
-    
-}
-)
+promise.addEventListener("change", function () {
+  button.disabled = !this.checked;
+  if (this.checked) {
+    button.classList.add("illuminated");
+    button.style.cursor = "pointer";
+  } else {
+    button.classList.remove("illuminated");
+    button.style.cursor = "not-allowed";
+  }
+});
+
+button.addEventListener("click", function () {
+  if (promise.checked) {
+    contenitore.classList.add("hidden");
+  } else {
+    btn.classList.remove("illuminated");
+  }
+});
