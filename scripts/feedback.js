b587c846e68249a50df9060bac5d5cc3a6a8ba81
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-04 17:28:10 +0200

    riptrova

---
+++ scripts/feedback.js
@@ -0,0 +1,49 @@
+let selectedStars = -1;
+
+function insertStar() {
+  let src = document.getElementById("star-container");
+
+  for (let i = 0; i < 10; i++) {
+    let img = document.createElement("img");
+
+    img.src = "assets/img/star.svg";
+    img.width = 75;
+    img.height = 65;
+    img.id = "star" + i;
+    img.style.filter = "opacity(0.1)";
+
+    img.addEventListener("mouseover", colorStar);
+    img.addEventListener("mouseout", resetStars);
+    img.addEventListener("click", selectStars);
+    src.appendChild(img);
+  }
+}
+
+function colorStar(event) {
+  let starId = event.target.id;
+  let starIndex = parseInt(starId.replace("star", ""));
+  for (let i = 0; i <= starIndex; i++) {
+    let star = document.getElementById("star" + i);
+    star.style.filter = "opacity(1)";
+  }
+}
+function resetStars() {
+  for (let i = 0; i < 10; i++) {
+    let star = document.getElementById("star" + i);
+    if (i > selectedStars) {
+      star.style.filter = "opacity(0.1)";
+    }
+  }
+}
+
+function selectStars(event) {
+  let starId = event.target.id;
+  let starIndex = parseInt(starId.replace("star", ""));
+  selectedStars = starIndex;
+  for (let i = 0; i <= starIndex; i++) {
+    let star = document.getElementById("star" + i);
+    star.style.filter = "opacity(1)";
+  }
+}
+
+window.onload = insertStar;
---
+++ scripts/scriptwelcome.js
@@ -0,0 +1,26 @@
+const promise = document.getElementById("promise");
+const button = document.getElementById("buttonLuigi");
+const contenitore = document.querySelector(".container");
+
+promise.addEventListener("change", function () {
+  button.disabled = !this.checked;
+  if (this.checked) {
+    button.classList.add("illuminated");
+    button.style.cursor = "pointer";
+  } else {
+    button.classList.remove("illuminated");
+    button.style.cursor = "not-allowed";
+  }
+});
+
+button.addEventListener("click", function () {
+  if (promise.checked) {
+    contenitore.classList.add("hidden");
+  } else {
+    btn.classList.remove("illuminated");
+  }
+});
+
+button.addEventListener("click", () => {
+  window.location.href = "test.html";
+});
