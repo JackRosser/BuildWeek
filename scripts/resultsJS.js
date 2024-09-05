b14c75651cad9257ae9fb25809504c37aade9c21
Giacomo Rossettini <rossegiac@gmail.com>
2024-09-05 09:16:02 +0200

    sistemazione

--- scripts/resultsJS.js
+++ scripts/resultsJS.js
@@ -13,6 +13,7 @@ const totDomande = localStorage.getItem("totDomande");
 // quante corrette e sbagliate
 let corrette = 0;
 let sbagliate = 0;
+let valGrafico = 0;
 
 // funzione che calcola la percentuale in base alle domande corrette
 function calcDomande(numC, numS) {
@@ -21,44 +22,35 @@ function calcDomande(numC, numS) {
 }
 calcDomande(risposteCorrette, risposteSbagliate);
 
-calcDomande(4, 2);
-//  inserisco i dati nelle risorse del grafico
-chartData.data[0] = sbagliate;
-chartData.data[1] = corrette;
+// ci calcoliamo il valore da inserire per modificare il grafico
+valGrafico = Math.ceil((360 * sbagliate) / 100);
+console.log(valGrafico);
 
-const percentCorrect = document.querySelectorAll(".percent span");
-percentCorrect[0].innerHTML = corrette + "%";
-percentCorrect[1].innerHTML = sbagliate + "%";
-console.log(percentCorrect[0].innerHTML);
+// inserisco i valori percentuali nel testo
+const percentCorrect = document.querySelectorAll(".percent");
+percentCorrect[0].innerText = corrette + "%";
+percentCorrect[1].innerText = sbagliate + "%";
+console.log(percentCorrect[0].innerText);
 
 // inserisco il resoconto domande azzeccate su tot nel testo sotto la percentuale
 const resoConto = document.querySelectorAll("figcaption");
 resoConto[0].innerText = `${risposteCorrette}/${totDomande} questions`;
 resoConto[1].innerText = `${risposteSbagliate}/${totDomande} questions`;
 
-new Chart(myChart, {
-  type: "doughnut",
-  data: {
-    labels: chartData.labels,
-    datasets: [
-      {
-        data: chartData.data,
-        backgroundColor: ["#C2128D", "#00FFFF"],
-        borderWidth: 0
-      }
-    ]
-  },
-  options: {
-    responsive: false,
-    maintainAspectRatio: false, // Rende il grafico più flessibile per l'adattamento alle dimensioni del contenitore
-    cutout: "70%", // Dimensione del buco interno per fare il donut
-    plugins: {
-      tooltip: {
-        enabled: false // Disabilita il tooltip
-      }
-    }
-  }
-});
+// creazione grafico a donut in base alle percentuale domande
+const grafico = document.getElementById("grafico");
+grafico.style.background = `conic-gradient(#d20094 ${valGrafico}deg, #00ffff ${valGrafico}deg 360deg)`;
+
+// se non ottieni almeno il 60% di risposte corrette non passi l'esame
+if (corrette < 60) {
+  console.log("non hai passato l'esame");
+  const titleArticle = document.querySelector("#textIntoG h5 span");
+  titleArticle.innerText = `Mission Failed Successfully!`;
+
+  const textArticle = document.querySelectorAll("#textIntoG p");
+  textArticle[0].innerText = `You didn't pass the exam, try again to get the certificate!`;
+  textArticle[1].innerText = ``;
+}
 
 // il bottone ti porta alla pagina di feedback
 const rateUs = document.getElementById("rateUs");
